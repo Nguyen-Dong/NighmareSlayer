56ea0f28393266b038b54eb95f6abcb3ef46a76c
Nguyen-Dong <5104dong@gmail.com>
2023-09-15 23:55:34 +0700

    c1 - di chuyen cho nhan vat

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+
+    public Vector3 moveInput;
+
+    private void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+        transform.position += moveInput * moveSpeed * Time.deltaTime;
+    }
+}
