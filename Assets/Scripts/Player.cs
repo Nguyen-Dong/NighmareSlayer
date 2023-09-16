bc0c6cc8ca240929d83f8837265b92fe7421bdc1
Nguyen-Dong <5104dong@gmail.com>
2023-09-16 14:20:58 +0700

    them animation cho nhan vat

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,13 +7,28 @@ public class Player : MonoBehaviour
     public float moveSpeed = 5f;
 
     private Rigidbody2D rb;
+    public Animator animator;
 
     public Vector3 moveInput;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         moveInput.x = Input.GetAxis("Horizontal");
         moveInput.y = Input.GetAxis("Vertical");
         transform.position += moveInput * moveSpeed * Time.deltaTime;
+
+        animator.SetFloat("Speed", moveInput.sqrMagnitude);
+
+        if (moveInput.x != 0)
+            if (moveInput.x > 0)
+                transform.localScale = new Vector3(1, 1, 0);
+            else
+                transform.localScale = new Vector3(-1, 1, 0);
     }
 }
+ 
